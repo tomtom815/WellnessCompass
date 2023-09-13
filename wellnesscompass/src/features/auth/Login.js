e09d206373b775af93db50387f83ca77b85b4a96
JamPo <jbpow18@icloud.com>
2023-09-13 18:43:34 -0400

    Installed statistics.js dependency. Wrote most of Login.js. Wrote AuthContext export

--- wellnesscompass/src/features/auth/Login.js
+++ wellnesscompass/src/features/auth/Login.js
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { useRef, useState, useEffect } from 'react';
+
+
 const Login = () => {
+    const usernameRef = useRef();
+    const errRef = useRef();
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false); // For tutorial only, will eventually route to new page upon successful login
+
+    useEffect(() => {
+        usernameRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [username, password]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+    }
+
     return(
-        <h1>Login</h1>
+        <section>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='username'>Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    ref={usernameRef}
+                    autoComplete='off'
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                    required
+                />
+                <label htmlFor='password'>Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    autoComplete='off'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required
+                />
+                <button>Sign In</button>
+            </form>
+            Need an Account?<br />
+            <span className="line">
+                {/*put router link here*/}
+                <a href="#">Sign Up</a>
+            </span>
+        </section>
     )
 }
 

330200f29280b50a27c974e393bcf78be9adf082
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2023-09-14 16:16:29 -0500

    Tweaked styles, added BMI

--- wellnesscompass/src/features/users/SingleUsersList.js
+++ wellnesscompass/src/features/users/SingleUsersList.js
@@ -53,7 +53,9 @@ const SingleUser = ({userName}) => {
                             <h2>Name: <span className="values">{user.firstName +" " +user.lastName}</span></h2>
                         
                             <h2>Username: <span className="values">{user.username}</span></h2>
+
                             
+                           
                         </section>
                         <section className="column-2">
                             <h3>Height: <span className="values">{user.height[user.height.length -1].value}</span></h3>
@@ -61,6 +63,8 @@ const SingleUser = ({userName}) => {
                             <h3>Last Weight: <span className="values">{user.weight[user.weight.length - 1].value}</span></h3>
                            
                             <h3>Average Steps: <span className="values">{(user.steps[user.steps.length - 1].value/user.steps.length).toFixed(2)}</span></h3>
+                            
+                            <h3>BMI: <span className="values">{(703 * (user.weight[user.weight.length - 1].value/Math.pow(user.height[user.height.length -1].value,2))).toFixed(2)}</span></h3>
                            
                         </section>
                     </div>

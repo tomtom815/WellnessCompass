1cdc5ca173688060c8c3c934214c4bd672151629
JamPo <jbpow18@icloud.com>
2023-09-23 14:28:03 -0400

    feat: Implement RequireAuth component for route authentication
    This commit adds the RequireAuth component, which enforces route
    authentication in the application. When a user is not authenticated, it
    redirects to the login page, preserving the original location. This
    component enhances the security and user experience of protected routes.

---
+++ wellnesscompass/src/components/RequireAuth.js
@@ -0,0 +1,27 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+import React from 'react';
+import { useAuth } from './AuthContext'; // Import the custom AuthContext hook for authentication.
+import { useLocation, Navigate, Outlet } from 'react-router-dom'; // Import necessary components from react-router-dom.
+
+// Define a functional component named RequireAuth.
+const RequireAuth = () => {
+  // Use the custom AuthContext hook to access the auth status.
+  const { auth } = useAuth();
+
+  // Use the useLocation hook from react-router-dom to get the current location.
+  const location = useLocation();
+
+  // Check if the user is authenticated (auth?.user is truthy).
+  // If authenticated, render the child components nested inside the <RequireAuth> component (the <Outlet />).
+  // If not authenticated, redirect to the login page ("/login") while preserving the current location (from: location).
+  // The "replace" prop replaces the current history entry instead of adding a new one.
+  return auth?.user ? (
+    <Outlet /> // Render the child components inside the outlet when authenticated.
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace /> // Redirect to the login page when not authenticated.
+  );
+}
+
+export default RequireAuth;

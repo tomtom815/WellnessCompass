624479d81f6a994c3a85b55808b0245c396bb60f
JamPo <jbpow18@icloud.com>
2023-09-23 16:31:32 -0400

    feat: Create useRefreshToken hook for token refreshing
    This commit introduces the 'useRefreshToken' hook, which provides functionality for refreshing user authentication tokens. This hook uses the 'axios' library to make a 'GET' request to the 'refresh' endpoint with credentials, ensuring a seamless token refresh process.
    
    Additionally, it utilizes the 'useAuth' hook to update the authentication state with the new access token received from the server. The previous state is logged for debugging purposes.
    
    This new hook enhances the maintainability and security of token handling in the application.

---
+++ wellnesscompass/src/hooks/useRefreshToken.js
@@ -0,0 +1,21 @@
+import axios from '../app/api/axios';
+import useAuth from './useAuth';
+
+const useRefreshToken = () => {
+  const { setAuth } = useAuth();
+
+  const refresh = async () => {
+    const response = await axios.get('refresh', {
+      withCredentials: true
+    });
+    setAuth(previousState => {
+      console.log(JSON.stringify(previousState));
+      console.log(response.data.accessToken);
+      return { ...previousState, accessToken: response.data.accessToken };
+    });
+    return response.data.accessToken;
+  }
+  return refresh;
+}
+
+export default useRefreshToken

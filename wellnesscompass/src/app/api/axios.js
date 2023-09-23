042b7d516ebed90ae5746bc7373a52a29fa94798
JamPo <jbpow18@icloud.com>
2023-09-23 16:43:58 -0400

    feat: Configure Axios Base URL for API Requests
    In this commit, we've set up a central configuration for Axios base URL to enhance maintainability and consistency of API requests. The `BASE_URL` constant has been introduced for easy management of the backend server's URL. This change ensures that all Axios instances across the application use the same base URL, improving code readability and reducing redundancy.
    
    - Created a `BASE_URL` constant to store the backend server's URL.
    - Updated Axios instances to use the `BASE_URL` constant for the base URL.
    - Introduced a new Axios instance, `axiosPrivate`, configured with JSON content type and credentials for private API requests.
    
    This enhancement streamlines API request handling, making it easier to manage and modify the base URL in the future.

--- wellnesscompass/src/app/api/axios.js
+++ wellnesscompass/src/app/api/axios.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
+const BASE_URL = 'http://localhost:3500'
 
 export default axios.create({
   // Port that Node.js backend is running on
-  baseURL: 'http://localhost:3500'
+  baseURL: BASE_URL
+});
+
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true
 });

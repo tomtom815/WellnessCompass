12b2436579be3d50923c93ad1ae4d80581e06c47
JamPo <jbpow18@icloud.com>
2023-09-23 14:49:28 -0400

    feat: Implement route protection using RequireAuth
    Added route protection using the RequireAuth component for nested routes under 'users'. This ensures that only authenticated users can access these routes.

--- wellnesscompass/src/App.js
+++ wellnesscompass/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
 import Layout from './components/Layout';
 import Public from './components/Public';
+import RequireAuth from './components/RequireAuth';
 import Register from './features/auth/Register';
 import Login from './features/auth/Login';
 import UsersList from './features/users/UsersList';
 import SingleUser from './features/users/SingleUsersList';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
+      {/* public routes */}
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="register" element={<Register />}/>
-        <Route path="login" element={<Login/>}/>
-        <Route path="users">
-            <Route index element={<UsersList />} />
-        </Route>
-        <Route path="users/:user">
-            <Route index element={<SingleUser />} />
+        <Route path="login" element={<Login />} />
+        
+        {/* We want to protect these routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="users">
+              <Route index element={<UsersList />} />
+          </Route>
+          <Route path="users/:user">
+              <Route index element={<SingleUser />} />
+          </Route>
         </Route>
       </Route>
     </Routes>

7bf51c9fd6bb107f6cfa6b5ebbe15ca58873ae71
JamPo <jbpow18@icloud.com>
2023-10-03 04:59:14 -0400

    Fix: typo in route

--- wellnesscompass/src/hooks/useRefreshToken.js
+++ wellnesscompass/src/hooks/useRefreshToken.js
@@ -5,13 +5,16 @@ const useRefreshToken = () => {
   const { setAuth } = useAuth();
 
   const refresh = async () => {
-    const response = await axios.get('refresh', {
+    const response = await axios.get('/refresh', {
       withCredentials: true
     });
     setAuth(previousState => {
       console.log(JSON.stringify(previousState));
       console.log(response.data.accessToken);
-      return { ...previousState, accessToken: response.data.accessToken };
+      return {
+        ...previousState,
+        accessToken: response.data.accessToken
+      };
     });
     return response.data.accessToken;
   }

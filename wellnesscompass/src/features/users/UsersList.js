087f87af9c9a3aea4ff4980cd05a7604032d5296
JamPo <jbpow18@icloud.com>
2023-10-05 05:58:43 -0400

    fix: clean whitespace. reformat table to remove error in console

--- wellnesscompass/src/features/users/UsersList.js
+++ wellnesscompass/src/features/users/UsersList.js
@@ -1,118 +1,105 @@
-import React, { useEffect, useState, useContext} from 'react'
-import axios from 'axios';
+import React, { useEffect, useState, useContext } from "react";
 
-import './userDisplays.css'
-import { averageMetric, averageWeeklyMetric, compare } from '../statistics/descriptiveStatistics';
+import "./userDisplays.css";
+import {
+  averageMetric,
+  averageWeeklyMetric,
+  compare,
+} from "../statistics/descriptiveStatistics";
 
-import { useNavigate, Link } from 'react-router-dom';
-import useLogout from '../../hooks/useLogout';
+import { useNavigate, Link } from "react-router-dom";
+import useLogout from "../../hooks/useLogout";
 
-//to display user data for the front end. 
-
-import { GetAllUsers } from '../../app/api/fetchAllUsers';
+//to display user data for the front end.
+import { GetAllUsers } from "../../app/api/fetchAllUsers";
 
 const UsersList = () => {
-    const navigate = useNavigate();
-    const logout = useLogout();
+  const navigate = useNavigate();
+  const logout = useLogout();
 
-    const signOut = async () => {
-        await logout();
-        navigate('/public');
-    }
+  const signOut = async () => {
+    await logout();
+    navigate("/login");
+  };
 
-    GetAllUsers();
-    const usersResult = JSON.parse(localStorage.getItem('usersData'));
-    
-    if(!usersResult)
+  GetAllUsers();
+  const usersResult = JSON.parse(localStorage.getItem("usersData"));
+
+  if (!usersResult)
     //wait for response
-        return <div>Loading...</div>
-  
-    const userNameArray = usersResult.map((user =>
-        user.username))
+    return <div>Loading...</div>;
 
+  const userNameArray = usersResult.map((user) => user.username);
 
-    const averageStepsArray= usersResult.map((user =>
-        averageWeeklyMetric(user.steps)))
-    const averageActivityArray = usersResult.map((user =>
-        averageWeeklyMetric(user.activeMinutes)))
-    
-    
-   
- 
-   
-    const averageValuesObject = userNameArray.map((username, index) => ({
-        username, averageSteps: averageStepsArray[index], averageActivity : averageActivityArray[index]
-    }))
+  const averageStepsArray = usersResult.map((user) =>
+    averageWeeklyMetric(user.steps)
+  );
+  const averageActivityArray = usersResult.map((user) =>
+    averageWeeklyMetric(user.activeMinutes)
+  );
 
-    
-   //create copy of array
-    const sortBySteps = averageValuesObject.slice()
-    const sortByMinutes = averageValuesObject.slice()
-   
-    //sort arrays
-    sortBySteps.sort((a,b) =>{
-        return b.averageSteps - a.averageSteps;
-    })
-    
-   sortByMinutes.sort((a,b) => {
-        return b.averageActivity - a.averageActivity;
-    })
-    
+  const averageValuesObject = userNameArray.map((username, index) => ({
+    username,
+    averageSteps: averageStepsArray[index],
+    averageActivity: averageActivityArray[index],
+  }));
 
-    
-    if(!sortBySteps || !sortByMinutes)
-        return <p>Loading...</p>
+  //create copy of array
+  const sortBySteps = averageValuesObject.slice();
+  const sortByMinutes = averageValuesObject.slice();
 
-    return(
-       <main>
+  //sort arrays
+  sortBySteps.sort((a, b) => {
+    return b.averageSteps - a.averageSteps;
+  });
 
-       <table id = "users">
-           <tr>
-                <th>Username</th>
-                <th>Average Weekly Steps</th>
+  sortByMinutes.sort((a, b) => {
+    return b.averageActivity - a.averageActivity;
+  });
+
+  if (!sortBySteps || !sortByMinutes) return <p>Loading...</p>;
+
+  return (
+    <main>
+      <table id="users">
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Average Weekly Steps</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortBySteps.map((user) => (
+            <tr key={user.username}>
+              <td>
+                <Link to={`/users/${user.username}`}>{user.username}</Link>
+              </td>
+              <td>{user.averageSteps}</td>
             </tr>
-            <tbody>
-        {sortBySteps.map((user => (
-        
-            <tr>
-                <td>
-                    <Link to={`/users/${user.username}`}>{user.username}</Link>
-                </td>
-                <td>{user.averageSteps}</td>
-            </tr>
-         
-        )) 
-
-    )}
-    
-    </tbody>
-    </table>
-    <table>
-        <tr>
+          ))}
+        </tbody>
+      </table>
+      <table>
+        <thead>
+          <tr>
             <th>Username</th>
             <th>Average Weekly Active Minutes</th>
-        </tr>
+          </tr>
+        </thead>
         <tbody>
-            {sortByMinutes.map((user => (
-        
-        <tr>
-            <td>
+          {sortByMinutes.map((user) => (
+            <tr key={user.username}>
+              <td>
                 <Link to={`/users/${user.username}`}>{user.username}</Link>
-            </td>
-            <td>{user.averageActivity}</td>
-        </tr>
-     
-    )) 
-
-)}
+              </td>
+              <td>{user.averageActivity}</td>
+            </tr>
+          ))}
         </tbody>
-            </table>
-            <button onClick={signOut}>Sign Out</button>
+      </table>
+      <button onClick={signOut}>Sign Out</button>
     </main>
-    )
+  );
+};
 
-        }
-             
-    
-
-export default UsersList
+export default UsersList;

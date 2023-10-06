b8cf59386968bb8d3af7a05bbeb379e304ca04d7
JamPo <jbpow18@icloud.com>
2023-10-06 06:00:58 -0400

    feat: add 'Trust This Device' button to JSX

--- wellnesscompass/src/features/auth/Login.js
+++ wellnesscompass/src/features/auth/Login.js
@@ -9,7 +9,7 @@ const LOGIN_URL = '/auth';
 
 
 const Login = () => {
-    const { setAuth } = useAuth();
+    const { setAuth, persist, setPersist } = useAuth();
 
     const navigate = useNavigate();
     const location = useLocation();
@@ -97,6 +97,14 @@ const Login = () => {
                     required
                 />
                 <button>Sign In</button>
+                <div className="persistCheck">
+                    <input
+                        type="checkbox"
+                        id="persist"
+                        onChange={togglePersist}
+                        checked={persist} />
+                    <label htmlFor="persist">Trust This Device</label>
+                </div>
             </form>
             Need an Account?<br />
             <span className="line">

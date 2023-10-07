81bbcaf5673f501cdbbb1201e42d82f7dddf3f65
JamPo <jbpow18@icloud.com>
2023-10-07 17:06:34 -0400

    feat: Introduce useInput custom hook for managing input states
    This initial commit adds the useInput custom hook, designed for handling input states with ease. It leverages the useLocalStorage hook to manage the input value, allowing for persistent storage across sessions. Additionally, it provides a reset function to revert the input to its initial value and an attributeObject for straightforward integration with input elements.

---
+++ wellnesscompass/src/hooks/useInput.js
@@ -0,0 +1,17 @@
+import useLocalStorage from './useLocalStorage';
+
+const useInput = (key, initValue) => {
+  const [value, setValue] = useLocalStorage(key, initValue);
+
+  const reset = () => setValue(initValue);
+
+  const attributeObject = {
+    value, 
+    onChange: (e) => setValue(e.target.value)
+  }
+
+  return [value, reset, attributeObject];
+
+}
+
+export default useInput

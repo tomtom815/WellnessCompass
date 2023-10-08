79f14010417aabc359e05a43053135e6da7b1b23
JamPo <jbpow18@icloud.com>
2023-10-08 11:14:28 -0400

    feat: add functionality to disable to react dev tools in production mode. This disables access to state, increasing security.

--- wellnesscompass/src/index.js
+++ wellnesscompass/src/index.js
@@ -4,9 +4,14 @@ import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 import {store} from './app/store'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

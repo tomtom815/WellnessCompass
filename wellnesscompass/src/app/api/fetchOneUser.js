a51225c0848c72b99daab87a5c23b79c4a9d9efb
tomtom815 <98099798+tomtom815@users.noreply.github.com>
2023-10-26 07:57:18 -0500

    Refresh data based on interval

--- wellnesscompass/src/app/api/fetchOneUser.js
+++ wellnesscompass/src/app/api/fetchOneUser.js
@@ -4,20 +4,31 @@ import { useParams } from 'react-router-dom';
 
 export const GetOneUser = ({ userName }) => {
   const { user } = useParams();
-  const url = `http://localhost:3500/users/${user}?_=${Date.now()}`;
+  const url = `http://localhost:3500/users/${user}`;
   const axiosPrivate = useAxiosPrivate();
   const [userResult, setUserResult] = useState(null); // Initialize as null
 
+  const fetchUserData = async () => {
+    try {
+      const response = await axiosPrivate.get(url, { params: userName });
+      console.log('response:', response.data);
+      setUserResult(response.data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
   useEffect(() => {
-    // ...
-    axiosPrivate.get(url, { params: userName })
-      .then((response) => {
-        console.log('response:', response.data)
-        setUserResult(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching user data:', error);
-      });
+    // Initial data fetch
+    fetchUserData();
+
+    // Set up an interval to fetch data every second (1000 milliseconds)
+    const intervalId = setInterval(fetchUserData, 1000);
+
+    // Clear the interval when the component unmounts to prevent memory leaks
+    return () => {
+      clearInterval(intervalId);
+    };
   }, [userName]);
 
   if (userResult === null) {
